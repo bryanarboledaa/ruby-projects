3ddc57ca77d10ad7e683c9e3f4418369846a3195
Bryan Arboleda <jbdarboleda@gmail.com>
2021-08-04 20:09:46 +0800

    sample_object

--- sample_object.rb
+++ sample_object.rb
@@ -1,15 +1,15 @@
 class Members 
-    def initialize(name, age, instrument)
-        @name = name
-        @age = age
-        @instrument = instrument
-    end
+  def initialize(name, age, instrument)
+    @name = name
+      @age = age
+      @instrument = instrument
+  end
 
-    def play
-        puts "Hey #{@name}, let's play!"
-    end
+  def play
+    puts "Hey #{@name}, let's play!"
+  end
 end
 
-    member1 = Members.new("Bryan," 25, "Drums")
-    member1.play
+member1 = Members.new("Bryan," 25, "Drums")
+member1.play
 
